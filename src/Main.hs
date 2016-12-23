48cc632a4e414a6ba3c9219dc3f0f11320057cab
Fernando Paredes <nano@fdp.io>
2016-12-22 21:13:51 -0800

    [Backend] Add basic backend that sends sample data

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,68 @@
+{-# LANGUAGE CPP           #-}
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Aeson
+import GHC.Generics
+import Data.List
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Control.Monad.Trans.Either
+
+data Artist = Artist
+  { artistId :: Int
+  , name :: String
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON Artist
+
+type ArtistAPI =
+      Get '[JSON] [Artist]
+  :<|> Capture "artistId" Int :> Get '[JSON] Artist
+
+artistsServer :: Server ArtistAPI
+artistsServer = getArtists :<|> artistOperations
+
+  where getArtists :: EitherT ServantErr IO [Artist]
+        getArtists = return artists
+
+        artistOperations artistId =
+          viewArtist
+
+          where viewArtist :: EitherT ServantErr IO Artist
+                viewArtist = artistById artistId
+
+artistById :: Int -> EitherT ServantErr IO Artist
+artistById idParam =
+  case a of
+    Nothing -> left (err404 {errBody = "No artist with given id exists"})
+    Just b -> return b
+  where
+    a = find ((== idParam) . artistId) artists
+
+artists :: [Artist]
+artists =
+  [ Artist 1 "Aventura"
+  , Artist 2 "Prince Royce"
+  , Artist 3 "Xtreme"
+  , Artist 4 "Antony Santos"
+  , Artist 5 "Monchy Y Alexandra"
+  , Artist 6 "Carlos y Alejandra"
+  , Artist 7 "Bachata Heightz"
+  ]
+
+type API = "artists" :> ArtistAPI
+
+api :: Proxy API
+api = Proxy
+
+app :: Application
+app = serve api artistsServer
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = run 8081 app
